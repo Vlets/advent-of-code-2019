c22dbd9791762116883b8c4f81a383ad9de9e79a
George Vletsas <george.vletsas@bloomreach.com>
2020-05-02 14:22:08 +0200

    [DAY 2] - Part 2

--- src/main/java/GravityAssistRestorationProgram.java
+++ src/main/java/GravityAssistRestorationProgram.java
@@ -11,18 +11,42 @@ public class GravityAssistRestorationProgram {
     public static void main(String[] args) throws FileNotFoundException {
         File inputFile = FileHelper.readFile(FILE_NAME);
         Scanner scanner = FileHelper.scanFile(inputFile);
-        List<Integer> opCode = new ArrayList<Integer>();
+        List<Integer> defaultOpCode = new ArrayList<Integer>();
 
         assert scanner != null;
         String[] inputArray = scanner.next().split(",");
 
         for (String inputNumber : Arrays.asList(inputArray)) {
-            opCode.add(Integer.parseInt(inputNumber));
+            defaultOpCode.add(Integer.parseInt(inputNumber));
         }
 
+        // Part 1
+        List<Integer> opCode = new ArrayList<Integer>(defaultOpCode);
+        calculateGravitationalOutput(opCode, 12, 2);
+        System.out.println("Part 1 answer: " + opCode.get(0));
+
+        // Part 2
+        System.out.println("Part 2 answer: " + calculatePartTwoAnswer(defaultOpCode));
+    }
+
+    private static int calculatePartTwoAnswer(final List<Integer> defaultOpCode) {
+        List<Integer> tempOpCode = new ArrayList<Integer>(defaultOpCode);
+        for (int noun = 0; noun<=99; noun++){
+            for (int verb = 0; verb<=99; verb++) {
+                Collections.copy(tempOpCode, defaultOpCode);
+                calculateGravitationalOutput(tempOpCode, verb, noun);
+                if (tempOpCode.get(0) == 19690720){
+                    return 100 * verb + noun ;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static void calculateGravitationalOutput(final List<Integer> opCode, int inputVerb, int inputNoun) {
         // Setup code
-        opCode.set(1,12);
-        opCode.set(2,2);
+        opCode.set(1,inputVerb);
+        opCode.set(2,inputNoun);
 
         for (int position = 0; position < opCode.size(); position += 4 ){
             int firstPosition = opCode.get(position + 1);
@@ -37,17 +61,12 @@ public class GravityAssistRestorationProgram {
                 result = opCode.get(firstPosition) * opCode.get(secondPosition);
                 opCode.set(targetPosition, result);
             }else if (opCode.get(position)==99){
-                System.out.println("Opcode 99 identified. Exiting...");
                 break;
             }else {
                 System.out.println("Something went wrong. Exiting...");
                 break;
             }
         }
-
-
-        System.out.println("Part 1 answer: " + opCode.get(0));
-
     }
 
 }
