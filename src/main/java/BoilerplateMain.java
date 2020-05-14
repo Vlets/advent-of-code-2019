c06ed7502db4976b55814d360a04ad6ab6ed0c25
George Vletsas <georgevle93@gmail.com>
2020-05-14 14:14:29 +0200

    [MAVEN] - Updated pom.xml, added boilerplate input file reader

--- src/main/java/BoilerplateMain.java
+++ src/main/java/BoilerplateMain.java
@@ -1,9 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+import Helpers.FileHelper;
+import static Helpers.Constants.FILE_NAME;
 
 public class BoilerplateMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = FileHelper.readFile(FILE_NAME);
+        Scanner scanner = FileHelper.scanFile(inputFile);
 
+        assert scanner != null;
     }
 
 
